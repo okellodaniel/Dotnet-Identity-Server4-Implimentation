4deaebd201f346517ebc0deb4cb16c8d64db7155
okello daniel <handsdani@gmail.com>
2022-08-15 14:05:44 +0300

    feat(Controllers): Added Response types to the controllers

--- Dotnet-Identity-Server4-Implimentation/Controllers/IssuesController.cs
+++ Dotnet-Identity-Server4-Implimentation/Controllers/IssuesController.cs
@@ -16,15 +16,30 @@ public class IssuesController : ControllerBase
    =>   _context = context;
 
     [HttpGet]
+    [ProducesResponseType(typeof(Issue),StatusCodes.Status200OK)]
+    [ProducesResponseType(typeof(Issue),StatusCodes.Status400BadRequest)]
     public async Task<IEnumerable<Issue>> Get()
        => await _context.Issues.ToListAsync();
 
     [HttpGet("id")]
+    [ProducesResponseType(typeof(Issue),StatusCodes.Status200OK)]
+    [ProducesResponseType(typeof(Issue),StatusCodes.Status404NotFound)]
     public async Task<IActionResult> GetByIdAsync(Guid id)
     {
         var issue = await _context.Issues.FindAsync(id);
         return issue == null ? NotFound() : Ok();
     }
-    
+
+    [HttpPost]
+    public async Task<IActionResult> CreateIssue(Issue issue)
+    {
+        await _context.Issues.AddAsync(issue);
+        await _context.SaveChangesAsync();
+
+        return CreatedAtAction(nameof(GetByIdAsync), new
+        {
+            id = issue.Id
+        }, issue);
+    }
 
 }
